312f6e55051fecb43baf495868b467f9dbffa2c8
Sushan Raj Shakya <sushanrajshakya@lftechnology.com>
2019-09-04 12:00:50 -0400

    master #time 30m Fixed the issue caused becuase of improper definition of image source before drawing the image in canvas

--- js/bottomScore.js
+++ js/bottomScore.js
@@ -9,11 +9,21 @@ class BotScoreBoard {
 
   drawBotScoreBoards() {
     this.ctx.beginPath();
-    this.ctx.font = 'bold 20px Arial'
-    this.ctx.strokeStyle = '#d7e163';
-    this.ctx.fillStyle = '#d7e163';
-    this.ctx.drawImage(this.bGBotImage,0,0,this.bGBotImage.width,this.bGBotImage.height,this.x,this.y,GAME_WIDTH,BOT_SCORE);
-    this.bGBotImage.src = 'images/bottomScore.png';
+    this.ctx.font = "bold 20px Arial";
+    this.ctx.strokeStyle = "#d7e163";
+    this.ctx.fillStyle = "#d7e163";
+    this.bGBotImage.src = "images/bottomScore.png";
+    this.ctx.drawImage(
+      this.bGBotImage,
+      0,
+      0,
+      this.bGBotImage.width,
+      this.bGBotImage.height,
+      this.x,
+      this.y,
+      GAME_WIDTH,
+      BOT_SCORE
+    );
     //this.ctx.fillText('+1',this.textX+this.x,this.textY+this.y);
     this.ctx.closePath();
   }
